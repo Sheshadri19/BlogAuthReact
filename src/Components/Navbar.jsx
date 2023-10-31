43e479b1970de5513cfe96c5d8cb671da9a8bce5
sheshadri19 <mondalsheshadri19@gmail.com>
2023-11-01 00:50:32 +0530

    upload

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -199,7 +199,7 @@ const Navbar = () => {
                 <>
                   <Tooltip title="Open settings">
                     <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                      <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                      <Avatar alt={auth.user.name} src="/static/images/avatar/2.jpg" />
                     </IconButton>
                   </Tooltip>
                   <Menu
